aa4c5f037dfa7be9989e423a2f59558b10f0e139
jambrose0 <jambrose0518@gmail.com>
2022-05-13 17:18:27 -0400

    initial offline features with indexDB up and running

--- public/js/idb.js
+++ public/js/idb.js
@@ -12,7 +12,7 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
   db = event.target.result;
   if (navigator.onLine) {
-    //uploadTransaction();
+    uploadFunds();
   }
 };
 //on error
@@ -28,3 +28,45 @@ function saveRecord(record) {
 
   moneyStore.add(record);
 }
+
+function uploadFunds() {
+  const transaction = db.transaction(["new_funds"], "readwrite");
+
+  const moneyStore = transaction.objectStore("new_funds");
+
+  const getAll = moneyStore.getAll();
+
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_funds"], "readwrite");
+          // access the new_pizza object store
+          const moneyStore = transaction.objectStore("new_funds");
+          // clear all items in your store
+          moneyStore.clear();
+
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+//listen for app coming back online
+window.addEventListener("online", uploadFunds);
